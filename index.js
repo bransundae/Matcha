0bd9212effb1e08dbb3a8cefbcda19e8cd53767a
bjohnso <bjohnso@student.wethinkcode.co.za>
2019-10-21 14:56:46 +0200

    Created Credentials for HTTPS server and Initialised Server with an Inital route to index

---
+++ index.js
@@ -0,0 +1,27 @@
+//Dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const https = require('https');
+const fs = require('fs');
+
+const app = express();
+
+//Load Routes
+
+//Index Route
+app.get('/', (req, res) => {
+    res.send('INDEX');
+})
+
+//Create Server HTTPS Credentials and Initialise Server
+const credentials = {
+    key: fs.readFileSync('./config/key.pem', 'utf8'),
+    cert: fs.readFileSync('./config/server.crt', 'utf8')
+}
+
+const port = process.env.PORT || 5000;
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
