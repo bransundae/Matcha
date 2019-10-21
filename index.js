f11baac55f37eb28f502489a96b36924fe60ab38
bjohnso <bjohnso@student.wethinkcode.co.za>
2019-10-21 15:34:42 +0200

    Removed https functionality to allow for Heroku Deploy

--- index.js
+++ index.js
@@ -13,8 +13,16 @@ app.get('/', (req, res) => {
     res.send('INDEX');
 })
 
+//Create and Initialise the Server
+
+const port = process.env.port || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+})
+
 //Create Server HTTPS Credentials and Initialise Server
-const credentials = {
+/*const credentials = {
     key: fs.readFileSync('./config/key.pem', 'utf8'),
     cert: fs.readFileSync('./config/server.crt', 'utf8')
 }
@@ -24,4 +32,4 @@ const httpsServer = https.createServer(credentials, app);
 
 httpsServer.listen(port, () => {
     console.log(`Server started on port ${port}`);
-});
+});*/
