38f9a1051558df1691651f93ceef1da431906544
bjohnso <bjohnso@student.wethinkcode.co.za>
2019-10-21 15:49:50 +0200

    start script added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 //Create and Initialise the Server
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
