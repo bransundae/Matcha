efe34e5622e1d1eff5dbbbc3b94db7cfac79757a
bjohnso <bjohnso@student.wethinkcode.co.za>
2019-10-21 15:51:48 +0200

    Reimplemented HTTPS

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ app.get('/', (req, res) => {
 
 //Create and Initialise the Server
 
-const port = process.env.PORT || 5000;
+/*const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
-})
+})*/
 
 //Create Server HTTPS Credentials and Initialise Server
-/*const credentials = {
+const credentials = {
     key: fs.readFileSync('./config/key.pem', 'utf8'),
     cert: fs.readFileSync('./config/server.crt', 'utf8')
 }
@@ -32,4 +32,4 @@ const httpsServer = https.createServer(credentials, app);
 
 httpsServer.listen(port, () => {
     console.log(`Server started on port ${port}`);
-});*/
+});
