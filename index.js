9f0e3e90e8225129b09d1a7c64cd7c578c1df743
bjohnso <bjohnso@student.wethinkcode.co.za>
2019-10-21 17:24:38 +0200

    Began constructing User Model

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ const passport = require('passport');
 const app = express();
 
 //Load Configs
+const keys = require('./config/keys');
 require('./config/passport')(passport); 
 
 //Load Routes
 const auth = require('./routes/auth');
+
+//Mongoose Connect
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB Connected')
+})
+.catch(err => console.log(err));
 
 //Index Route
 app.get('/', (req, res) => {
