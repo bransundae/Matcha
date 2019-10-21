aac6fa84e5bae4f1c042c203f6b5d9d0be5351ca
bjohnso <bjohnso@student.wethinkcode.co.za>
2019-10-21 15:53:04 +0200

    Removed HTTPS

--- index.js
+++ index.js
@@ -15,21 +15,8 @@ app.get('/', (req, res) => {
 
 //Create and Initialise the Server
 
-/*const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
-})*/
-
-//Create Server HTTPS Credentials and Initialise Server
-const credentials = {
-    key: fs.readFileSync('./config/key.pem', 'utf8'),
-    cert: fs.readFileSync('./config/server.crt', 'utf8')
-}
-
-const port = process.env.PORT || 5000;
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-});
+})
