a1745fd806dda5e9e5f5f916fe794b37fe7f78a4
bjohnso <bjohnso@student.wethinkcode.co.za>
2019-10-22 23:11:00 +0200

    Completed user Authentication and Built Welcome Page Front End

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 //Dependencies
+const exphbs = require('express-handlebars');
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
+//Application
 const app = express();
-
-//Globals
-app.use((req, res, next) => {
-    res.locals.user = req.user || null;
-    next();
-});
 
 //Load Models
 require('./models/User');
 
 //Load Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 app.use(cookieParser());
 app.use(session({
     secret: 'secret',
@@ -26,12 +26,19 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+//Globals
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+});
+
 //Load Configs
 const keys = require('./config/keys');
 require('./config/passport')(passport); 
 
 //Load Routes
 const auth = require('./routes/auth');
+const index = require('./routes/index');
 
 //Mongoose Connect
 mongoose.Promise = global.Promise;
@@ -43,13 +50,9 @@ mongoose.connect(keys.mongoURI, {
 })
 .catch(err => console.log(err));
 
-//Index Route
-app.get('/', (req, res) => {
-    res.send('INDEX');
-})
-
 //Use Routes
 app.use('/auth', auth);
+app.use('/', index);
 
 //Create and Initialise the Server
 
